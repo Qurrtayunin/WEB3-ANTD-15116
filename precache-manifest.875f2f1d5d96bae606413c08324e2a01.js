c6f4a1bd24afd6fa169c9cf79ccc028196832e18
Qurrota Ayunin <qurrotaayunin36@gmail.com>
2020-03-30 16:25:10 +0700

    Updates

---
+++ precache-manifest.875f2f1d5d96bae606413c08324e2a01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a1390e5552249c79afa2dcd584fbc6",
+    "url": "/WEB3-ANTD-15116/index.html"
+  },
+  {
+    "revision": "415e9e8225107297739a",
+    "url": "/WEB3-ANTD-15116/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "3eeb36797ffe3cead3f4",
+    "url": "/WEB3-ANTD-15116/static/css/main.8f6cf0d7.chunk.css"
+  },
+  {
+    "revision": "415e9e8225107297739a",
+    "url": "/WEB3-ANTD-15116/static/js/2.94236315.chunk.js"
+  },
+  {
+    "revision": "176c1a1a28b19c611d9701905d8d60b7",
+    "url": "/WEB3-ANTD-15116/static/js/2.94236315.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eeb36797ffe3cead3f4",
+    "url": "/WEB3-ANTD-15116/static/js/main.8c335504.chunk.js"
+  },
+  {
+    "revision": "61f3e45df6ebef96734f",
+    "url": "/WEB3-ANTD-15116/static/js/runtime-main.860bb33d.js"
+  }
+]);
